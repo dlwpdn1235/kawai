c6bbfdf12792e5ae6a38a58dc74b16c5cf15b6dd
Administrator <Administrator@402-14>
2023-06-23 13:10:31 +0900

    씅재1

--- src/test/java/kawai_project/test.java
+++ src/test/java/kawai_project/test.java
@@ -1,32 +1,66 @@
 package kawai_project;
 
-import javax.sql.DataSource;
+import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.kawai.dao.MarketProductMapper;
+import com.kawai.dto.MarketDto;
+import com.kawai.dto.UserDto;
+import com.kawai.service.MarketService;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:config/root-context.xml")
-@Ignore
+@ContextConfiguration(locations="classpath:config/*-context.xml")
 public class test {
 	@Autowired
-	ApplicationContext context;
+	MarketProductMapper dao;
 	
-	JdbcTemplate jdbc;
 	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		jdbc = new JdbcTemplate(dataSource);
+	MarketService service;
+	
+	@Autowired
+	UserDto user;
+	
+	@Test @Ignore
+	public void test0() {
+		MarketDto dto = new MarketDto();
+		dto.setmTitle("3");
+		dto.setmContent("내용");
+		dto.setMarket_id(1);
+		System.out.println(dao.marketUpdate(dto));
+	}
+	
+	@Test @Ignore
+	public void test1() {
+	    MarketDto dto = new MarketDto();
+	    dto.setMarket_id(1);
+	    int result = dao.marketDelete(dto.getMarket_id());
+	    System.out.println(result);
 	}
 	
 	@Test
-	public void test0() {
-		System.out.println(jdbc.query("select * from test", ((rs,index)->rs.getInt(1)) ) );
-		
+	public void test2() {
+	MarketDto dto = new MarketDto();
+	dto.setmTitle("제목");
+	dto.setmContent("내용");
+	dto.setmIp("아피");
+	dto.setmPrice(3000);
+	dto.setmDate("2023-06-23");
+	dto.setUser(user.);
+	int result = dao.marketInsert(dto);
+	System.out.println(result);
+	}
+	
+	@Test @Ignore
+	public void test3() {
+	    List<MarketDto> marketList = dao.marketList();
+	    for (MarketDto dto : marketList) {
+	        System.out.println(dto);
+	    }
 	}
 }
