e2d758540b600f895cde241a6c7820ef536a6186
wonsu <kimwonsu007@gmail.com>
2023-07-03 18:16:14 +0900

    DAO 끝 controller진행시작

--- src/main/java/com/kawai/dao/AccountUserDao.java
+++ src/main/java/com/kawai/dao/AccountUserDao.java
@@ -7,14 +7,19 @@ import com.kawai.dto.AccountUserVO;
 public interface AccountUserDao {
 	// 회원가입 accountUserInsert
 	public int accountUserInsert(AccountUserVO user);
+	
 	// 로그인 accointLogin
-	public AccountUserVO accointLogin(AccountUserVO user);
+	public AccountUserVO accountLogin(AccountUserVO user);
+	
 	// 회원정보 상세보기 accountUserRead
 	public AccountUserVO accountUserRead(String id);
+	
 	// 회원목록 리스트 accountUserList
 	public List<AccountUserVO> accountUserList();
+	
 	// 회원정보 수정 accountUserUpdate
 	public int accountUserUpdate(AccountUserVO user);
+	
 	// 회원탈퇴 accountUserDelete
 	public int accountUserDelete(AccountUserVO user);
 }
